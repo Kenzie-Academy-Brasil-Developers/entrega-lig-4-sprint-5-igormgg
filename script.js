2b5c36bf63b7afa24e8f1e45f8ec529e5e7da2f5
Bruno <ferreira.bruno.oliveira@gmail.com>
2021-08-09 19:53:45 -0300

    finalizado feature/funcaoColcoarDisco

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //VARIAVEIS GLOBAIS
 const tabela = document.querySelector('#tabela')
+let player = 1;
 //VARIAVEIS GLOBAIS
 
 //FUNÇÕES
@@ -12,22 +13,43 @@ const vitoriaJogador2 = () => {
     const divVitoria = document.querySelector('#vitoriaJogador2')
     divVitoria.classList.remove('hidden')
 }
+const criarDisco = (destino,player) =>{
+    let disco = document.createElement('div');
+        disco.classList.add('disco') 
+        player%2 !== 0? disco.classList.add('jogador1') : disco.classList.add('jogador2');
+        destino.appendChild(disco);
+        return true;
+}
+const colocarDisco = (evt) =>{
+    let status = false;
+    let destino = '';
+    let arr = [...evt.currentTarget.childNodes];
+        arr = arr.reverse();
+       for(let i=0;i<arr.length;i++){
+        if(arr[i].lastElementChild == null){
+            destino = arr[i];
+            i=arr.length;
+        }
+       }
+    status = criarDisco(destino,player);
+    if(status == true){
+        player++;
+    }else{
+        alert('error: Recarregue a pagina');//depois mudar para um modal
+    }
+}
 
-const criacaoTabela = () =>{
+const criacaoTabela = () =>{ 
     for(let coluna = 0; coluna < 7; coluna++){
             let novaColuna = document.createElement('div')
             novaColuna.classList.add(`colunas`)
             novaColuna.id = (`coluna${coluna+1}`)
-            // novaColuna.addEventListener('click', (evt) =>{ os events podem ser adicionados aqui para a coluna descomente para ver no console.
-            //     console.log(evt.currentTarget)
-            // })
             tabela.appendChild(novaColuna);
-            
+            novaColuna.addEventListener('click', (evt) =>colocarDisco(evt));
         for(let celula = 0; celula < 6; celula++){
             let novaCelula = document.createElement('div')
             novaCelula.classList.add('celulas')
             novaCelula.dataset.coluna = `${coluna}`
-            novaCelula.dataset.celula= `${celula}`
             // novaCelula.addEventListener('click', (evt) =>{ os events podem ser adicionados aqui para a celula descomente para ver no console.
             //     console.log(evt.target)
             // })
@@ -36,5 +58,7 @@ const criacaoTabela = () =>{
         }
     }
 }
+
+
 criacaoTabela()
 //FUNÇÕES
