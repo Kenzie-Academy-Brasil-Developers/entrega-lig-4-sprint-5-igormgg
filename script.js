5fb96c3cde0c4a471551dd93ec5d5992590543c2
Bruno <ferreira.bruno.oliveira@gmail.com>
2021-08-10 12:26:01 -0300

    finalizado atualzacao

--- script.js
+++ script.js
@@ -14,6 +14,15 @@ const vitoriaJogador2 = () => {
     const divVitoria = document.querySelector('#vitoriaJogador2')
     divVitoria.classList.remove('hidden')
 }
+const colunaCheia = () =>{
+    let query = document.getElementById('tabela');
+        query.classList.add('cheio');
+        navigator.vibrate(200);//teste em mobiles
+        setTimeout(function(){
+            query.classList.remove('cheio')
+        },500);
+        
+}
 const criarDisco = (destino,player) =>{
     let disco = document.createElement('div');
         disco.classList.add('disco') 
@@ -24,29 +33,34 @@ const criarDisco = (destino,player) =>{
 const colocarDisco = (evt) =>{
     let status = false;
     let destino = '';
+    let primeiraCelula = evt.currentTarget.firstElementChild;
     let arr = [...evt.currentTarget.childNodes];
         arr = arr.reverse();
-       for(let i=0;i<arr.length;i++){
-        if(arr[i].lastElementChild == null){
-            destino = arr[i];
-            i=arr.length;
+        if(primeiraCelula.firstElementChild !== null){
+            colunaCheia();
+            return false
         }
+        for(let i=0;i<arr.length;i++){
+            if(arr[i].lastElementChild == null){
+                destino = arr[i];
+                i=arr.length;
+            }
        }
-    status = criarDisco(destino,player);
-    if(status == true){
-        player++;
-    }else{
-        alert('error: Recarregue a pagina');//depois mudar para um modal
-    }
+        status = criarDisco(destino,player);
+        if(status == true){
+            player++;
+        }
 }
-
+const jogada = (evt) =>{
+     colocarDisco(evt);
+}
 const criacaoTabela = () =>{ 
     for(let coluna = 0; coluna < 7; coluna++){
             let novaColuna = document.createElement('div')
             novaColuna.classList.add(`colunas`)
             novaColuna.id = (`coluna${coluna+1}`)
             tabela.appendChild(novaColuna);
-            novaColuna.addEventListener('click', (evt) =>colocarDisco(evt));
+            novaColuna.addEventListener('click', (evt) =>jogada(evt));
         for(let celula = 0; celula < 6; celula++){
             let novaCelula = document.createElement('div')
             novaCelula.classList.add('celulas')
@@ -54,8 +68,7 @@ const criacaoTabela = () =>{
             // novaCelula.addEventListener('click', (evt) =>{ os events podem ser adicionados aqui para a celula descomente para ver no console.
             //     console.log(evt.target)
             // })
-            novaColuna.appendChild(novaCelula)
-            
+            novaColuna.appendChild(novaCelula)   
         }
     }
 }
