6ad319be1b7e516563f5237043b5f058dd47a31a
Bruno <ferreira.bruno.oliveira@gmail.com>
2021-08-10 22:34:42 -0300

    corrigindo bug

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ const colocarDisco = (evt) =>{
 }
 const bemVindo = () =>{
     let buttonJogar = document.querySelector('.btn__jogar');
-    let busca = document.querySelector('.regras');
+    let busca = document.querySelector('.conteudo__regras');
     let buscaBody = document.getElementsByTagName('body')[0];
         buscaBody.classList.add('esconder');
         busca.classList.remove('hidden');
