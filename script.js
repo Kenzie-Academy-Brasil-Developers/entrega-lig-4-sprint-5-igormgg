b115031bba7fe82229d7c21c0f1e10c61fb19eae
Bruno <ferreira.bruno.oliveira@gmail.com>
2021-08-12 00:27:41 -0300

    feat: cricao vitpria em bug

--- script.js
+++ script.js
@@ -6,6 +6,139 @@ let player = 1;
 
 //FUNÇÕES
 
+// VITORIA
+
+const checaVencedorDiagonais = (arrDiagonal) =>{
+    let vencedor = 0;
+    let countOne = 0;
+    let countTwo = 0;
+    for(let i=0; i<arrDiagonal.length;i++){
+        if(arrDiagonal[i] == 1){
+            countTwo= 0;
+            countOne++;
+        } else if(arrDiagonal[i] == 2){
+            countOne = 0;
+            countTwo++;
+        }
+        if(countOne == 4){
+            return console.log('jogador1 venceu');
+        }else if(countTwo == 4){
+            return console.log('jogador2 venceu');
+        }
+    }
+}
+
+const codigoJogador = (jogador) =>{
+    if(jogador == 'jogador1'){
+        return 1;
+    }else if(jogador == 'jogador2'){
+        return 2;
+    }
+}
+const mapear = () =>{
+    let busca = document.querySelector('#tabela')
+    let arrTabela = busca.childNodes;
+    let newArr = [];
+    for(let i=0;i<arrTabela.length;i++){
+        let arr = arrTabela[i].childNodes;
+        newArr [i] =[]
+        for(let j=0;j<arr.length;j++){
+            let filho =arr[j].firstChild;
+            if(filho !== null){
+                newArr[i][j] = codigoJogador(filho.classList[1])
+            }
+            if(filho == null){
+                newArr[i][j]=0;
+            }
+        }
+    }
+    newArr = newArr.map( (arr)=>arr.reverse() );
+    return newArr;
+}
+
+const percorrer = (mapa,arr) =>{
+    let arrTeste = [];
+    let count = 0;
+    let count2=0;
+    console.log(mapa)
+    for(let item=1;item<arr.length-2;item++){
+        // count = count2;
+        arrTeste[item]=[]
+        for(let i=0;i<mapa.length;i++){
+            arrTeste[item].push(mapa[i][count]);
+            count++;
+        }
+      count=item;
+        // console.log(arrTeste)
+        
+    }
+    return arrTeste
+    // console.log(arrTeste);
+    // return arrTeste
+    // console.log(arr)
+    // checaVencedorDiagonais(arrTeste);
+    // return arrTeste;
+    // return arrTeste
+}
+
+const checaDiagonalDireita = (newCodigoJogadores) => {
+   let mapa = mapear()
+   console.log(mapa)
+//    let contador = mapa.length-3;
+//    let count = 0;
+   let arrDiagonalDireita = [];
+        for(let i=0;i<6;i++){
+            let arr = mapa[i];
+            let arrFinal = percorrer(mapa,arr)
+            console.log(arrFinal);
+        }
+}
+
+const buscarElement = (evt)=>{
+    let pai = evt.currentTarget.parentElement;
+    let elements = [...pai.childNodes];
+    let jogadores=[];
+    for( let coluna=0; coluna<elements.length; coluna++){
+        let arrColuna = elements[coluna];
+        let filhos = arrColuna.childNodes;
+        jogadores[coluna]=[]
+        for(let linha = 0;linha<filhos.length;linha++){
+            let status = filhos[linha].children[0]; //null ou elemento
+            if (status !== undefined){
+                jogadores[coluna].unshift(status.classList[1]);
+                elementoColunaTest(jogadores[coluna]);
+               
+            }
+        }
+       
+    }
+    // checaHorizontal(jogadores)
+   let  newCodigoJogadores =  codigoJogador(jogadores);
+        // console.log(newCodigoJogadores)
+        checaDiagonalDireita();
+        // checaDiagonalEsquerda(newCodigoJogadores);
+
+}
+const elementoColunaTest =(jogador)=>{
+let contadorJogador1=0;
+let contadorJogador2=0;
+    for(let contador=0;contador<jogador.length;contador++)
+        if(jogador[contador]=='jogador1'){
+            contadorJogador1++;
+            contadorJogador2=0;
+    }else if(jogador[contador]=="jogador2"){
+        contadorJogador2++
+        contadorJogador1=0;
+    }
+    if(contadorJogador1===4){
+        console.log("jogador1 venceu!")
+    }else if(contadorJogador2===4){
+        console.log("jogador2 venceu")
+    }
+}
+
+// VITORIA
+
 const vitoriaJogador1 = () => {
     const divVitoria = document.querySelector('#vitoriaJogador1')
     divVitoria.classList.remove('hidden')
@@ -35,6 +168,7 @@ const colocarDisco = (evt) =>{
     let destino = '';
     let primeiraCelula = evt.currentTarget.firstElementChild;
     let arr = [...evt.currentTarget.childNodes];
+  
         arr = arr.reverse();
         if(primeiraCelula.firstElementChild !== null){
             colunaCheia();
@@ -50,6 +184,7 @@ const colocarDisco = (evt) =>{
         if(status == true){
             player++;
         }
+        buscarElement(evt);
 }
 const bemVindo = () =>{
     let buttonJogar = document.querySelector('.btn__jogar');
@@ -68,21 +203,19 @@ const bemVindo = () =>{
 const jogada = (evt) =>{
     colocarDisco(evt);
 }
-const criacaoTabela = () =>{ 
+const criacaoTabela = () =>{
     for(let coluna = 0; coluna < 7; coluna++){
             let novaColuna = document.createElement('div')
             novaColuna.classList.add(`colunas`)
-            novaColuna.id = (`coluna${coluna+1}`)
+            novaColuna.id = (`coluna${coluna}`)
             tabela.appendChild(novaColuna);
-            novaColuna.addEventListener('click', (evt) =>jogada(evt));
+            novaColuna.dataset.coluna = `${coluna}`;
+            novaColuna.addEventListener('click', (evt) =>colocarDisco(evt));
         for(let celula = 0; celula < 6; celula++){
             let novaCelula = document.createElement('div')
             novaCelula.classList.add('celulas')
-            novaCelula.dataset.coluna = `${coluna}`
-            // novaCelula.addEventListener('click', (evt) =>{ os events podem ser adicionados aqui para a celula descomente para ver no console.
-            //     console.log(evt.target)
-            // })
-            novaColuna.appendChild(novaCelula)   
+            novaCelula.dataset.celula = `${celula}`
+            novaColuna.appendChild(novaCelula)
         }
     }
     btnReset.addEventListener('click',reset)
@@ -97,4 +230,8 @@ const reset = () => {
 
 bemVindo();
 criacaoTabela();
+
+
+
 //FUNÇÕES
+
