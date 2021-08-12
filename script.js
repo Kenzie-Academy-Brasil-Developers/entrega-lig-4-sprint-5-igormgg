f43036ec5feb1a7087dde554b8935c28b0eb5d63
gelosde <85689579+gelosde@users.noreply.github.com>
2021-08-12 16:56:37 -0300

    fix: linhas corrigido

--- script.js
+++ script.js
@@ -2,10 +2,146 @@
 const tabela = document.querySelector('#tabela')
 const btnReset = document.querySelector('#reset')
 let player = 1;
-
+let play1 =document.createElement('p')
+play1.id ='joga1'
+document.body.appendChild(play1);
+let play2 =document.createElement('p')
+play2.id ='joga2'
+document.body.appendChild(play2);
 //VARIAVEIS GLOBAIS
+const turnoJogador =() =>{
+if( play1.style.display==='block'){
+    play1.style.display='none'
+    play2.style.display='block'
+}else{
+    play1.style.display='block'
+    play2.style.display='none'
+}
+}
+//FUNÇÕES
+const checaVencedorDiagonais = (arrDiagonal) =>{
+    let vencedor = 0;
+    let countOne = 0;
+    let countTwo = 0;
+    for(let i=0; i<arrDiagonal.length;i++){
+        if(arrDiagonal[i] == 1){
+            countTwo= 0;
+            countOne++;
+        } else if(arrDiagonal[i] == 2){
+            countOne = 0;
+            countTwo++;
+        }
+        if(countOne == 4){
+            return vitoriaJogador1();
+        }else if(countTwo == 4){
+            return vitoriaJogador2();
+        }
+    }
+}
+const codigoJogador = (jogador) =>{
+    if(jogador == 'jogador1'){
+        return 1;
+    }else if(jogador == 'jogador2'){
+        return 2;
+    }
+    else {
+        return 0;
+    }
+}
+const mapear = () =>{
+    let busca = document.querySelector('#tabela')
+    let arrTabela = busca.childNodes;
+    let newArr = [];
+    for(let i=0;i<arrTabela.length;i++){
+        let arr = arrTabela[i].childNodes;
+        newArr [i] =[]
+        for(let j=0;j<arr.length;j++){
+            let filho =arr[j].firstChild;
+            if(filho !== null){
+                newArr[i][j] = codigoJogador(filho.classList[1])
+            }
+            if(filho == null){
+                newArr[i][j]=0;
+            }
+        }
+    }
+    newArr = newArr.map( (arr)=>arr.reverse() );
+    return newArr;
+}
+const percorrerLinha = (mapa,linha) =>{
+    let arrMult = []
+    let aux = 0;
+            for(let q = 0; q < 4; q++){
+                arrMult[q] = [];
+                if(q === 0){
+                    for(let k = 0; k < 6; k++){
+                        
+                    arrMult[q].push(mapa[k][k+linha]) //mapa [k]linha [k]coluna
+                    }
+                }
+                if(q === 1){
+                    for(let k = 0; k < 6; k++){
+                        arrMult[q].push(mapa[k+1][k+linha])
+                    }
+                }
+                if (q === 2){
+                    for(let k = 0; k < 5; k++){
+                        arrMult[q].push(mapa[k+2][k+linha])
+                    }
+                }
+                if (q === 3){
+                    for(let k = 0; k < 4; k++){
+                        arrMult[q].push(mapa[k+3][k+linha])
+                    }
+                }    
+            }
+            let resultado = '';
+            for(let i=0;i<arrMult.length;i++){
+               resultado = checaVencedorDiagonais(arrMult[i])
+            }
+    return resultado;
+}
+const checaDiagonalEsquerda = () =>{
+    let resultado = undefined;
+    let mapa = mapear();
+        mapa.reverse();
+       for(let i=0;i<mapa.length-3;i++){
+        resultado = percorrerLinha(mapa,i); 
+       }
+    return resultado;
+}
+const checaDiagonalDireita = () => {
+    let resultado = '';
+    let mapa = mapear();
+       for(let i=0;i<mapa.length-3;i++){
+        resultado = percorrerLinha(mapa,i); 
+       }      
+    return resultado;
+}
+//xondição de empate
+const testaEmpate = (evt) =>{
+ let contador =0;
 
+    for(let linha=0;linha<6;linha++){
 
+        for(let coluna=0;coluna<7;coluna++){
+
+         let teste = document.querySelector(`[data-coluna='${coluna}'][data-celula='${linha}']`)
+         
+         if(teste.firstChild===null){
+            contador =0;
+         }else if(teste.firstChild!==null){  
+            contador++
+        }
+        if(contador===42){
+            empateJogadores()
+        }
+    }
+}
+}
+const empateJogadores =() =>{
+    console.log("empate")
+}
 //buscar elemento linha
 const buscarElementlinha = (evt) =>{
     let contadorJogador1=0;
@@ -14,20 +150,18 @@ const buscarElementlinha = (evt) =>{
 
         for(let coluna=0;coluna<7;coluna++){
 
-         let teste = document.querySelector(`[data-coluna='${coluna}'][data-celula='${linha}']`)
+        let teste = document.querySelector(`[data-coluna='${coluna}'][data-celula='${linha}']`)
          
          if(teste.firstChild===null){
-          contadorJogador2=0;
-          contadorJogador1=0;
+        contadorJogador2=0;
+        contadorJogador1=0;
          }else if(teste.firstChild!==null){  
          let  valores= teste.firstChild.classList[1];
             if(valores=='jogador1'){
             contadorJogador1++;
-            console.log("jogador 1: "+contadorJogador1)
             contadorJogador2=0;                
             }else if(valores=='jogador2'){
                 contadorJogador2++;
-                console.log("jogador2 :"+contadorJogador2)
                 contadorJogador1-=contadorJogador1;
             }
             if(contadorJogador1===4){
@@ -37,9 +171,12 @@ const buscarElementlinha = (evt) =>{
             }
         
         }
-    }
+      
+    }   contadorJogador2=0;
+        contadorJogador1=0;
 }
 }
+
 //busca os elementos por coluna
 
 const buscarElementColuna = (evt)=>{
@@ -87,6 +224,7 @@ let contador2=0;
 }
 }
 //fim da busca dos elementos por coluna
+
 const vitoriaJogador1 = () => {
     const divVitoria = document.querySelector('#vitoriaJogador1')
     divVitoria.classList.remove('hidden')
@@ -101,8 +239,7 @@ const criarDisco = (destino,player) =>{
         disco.classList.add('disco') 
         player%2 !== 0? disco.classList.add('jogador1') : disco.classList.add('jogador2');
         destino.appendChild(disco);
-
-      return true;
+        return true;
 }
 const colocarDisco = (evt) =>{
     let status = false;
@@ -121,8 +258,11 @@ const colocarDisco = (evt) =>{
     if(status == true){
         player++;
     }
+    
     buscarElementlinha(evt);
     buscarElementColuna(evt);
+    testaEmpate(evt);
+    turnoJogador();
 }
 const bemVindo = () =>{
     let buttonJogar = document.querySelector('.btn__jogar');
@@ -130,21 +270,24 @@ const bemVindo = () =>{
     let buscaBody = document.getElementsByTagName('body')[0];
         buscaBody.classList.add('esconder');
         buttonJogar.addEventListener('click',function(){
+            buttonJogar.firstChild.classList.remove('voar')
+            buttonJogar.firstChild.classList.add('nuvemPartiu')
             music()
             buscaBody.classList.add('fade');
             setTimeout(function(){
                 buscaBody.classList.remove('esconder');
                 busca.classList.add('hidden');
-                buscaBody.classList.add('fadeIn'); 
-
-            },500);
+                buscaBody.classList.add('fadeIn');    
+            },1000);
         } );
+       
 }
 const jogada = (evt) =>{
     colocarDisco(evt);
 }
 
 const criacaoTabela = () =>{ 
+  
     for(let coluna = 0; coluna < 7; coluna++){
             let novaColuna = document.createElement('div')
             novaColuna.classList.add(`colunas`)
@@ -160,7 +303,8 @@ const criacaoTabela = () =>{
             novaColuna.appendChild(novaCelula)
            
         }
-    }
+    }  
+   
     btnReset.addEventListener('click',reset)
 }
 const reset = () => {
@@ -169,13 +313,22 @@ const reset = () => {
     resetAll.forEach(element => {
         element.remove()
     });
+    play1.style.display ='block';
+    play2.style.display ='none';
 }
+
+const alternarAnimacaoNuvem = () =>{
+    let query = document.querySelector('.button__iniciar ')
+        query.classList.remove('nuvemChegada')
+        query.classList.add('voar')
+} 
+setTimeout(()=>{alternarAnimacaoNuvem()},2000 )
 const music = () => {
     const audio = document.querySelector('audio')
     audio.play()
 }
 
+//FUNÇÕES
+
 criacaoTabela()
 bemVindo();
-
-//FUNÇÕES
